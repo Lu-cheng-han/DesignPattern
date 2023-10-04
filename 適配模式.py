8270bb95ea21e5bc5d6c00a7c8159ab61fd20057
RD2生醫事業部研發-呂承翰 <hank.lu@omeco.local>
2023-10-04 15:36:20 +0800

    HK-適配器模式更新例子

--- 適配模式.py
+++ 適配模式.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class Target:
 
     def Request(self):
@@ -14,6 +16,35 @@ class Adapter(Target):
     def Request(self):
         self.adaptee.SpecificRequest()
 
+class HeroInterface(ABC):
+
+    @abstractmethod
+    def attack(self):
+        pass
+
+class MyHero(HeroInterface):
+    
+    def attack(self):
+        print("自家英雄攻擊")
+
+class UnionHero:
+
+    def touch(self):
+        print("聯名英雄攻擊",flush=True)
+
+class HeroAdapter(HeroInterface):
+    union_hero = None
+
+    def __init__(self, union: 'UnionHero') -> None:
+        super().__init__()
+        self.union_hero = union
+
+
+    def attack(self):
+        self.union_hero.touch()
+
 if __name__ == "__main__":
-    target = Adapter()
-    target.Request()
+    hero = MyHero()
+    hero.attack()
+    union = HeroAdapter(UnionHero())
+    union.attack()
