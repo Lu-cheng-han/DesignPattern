23630369d6120aaaddf963b4ff9088a787d94880
RD2生醫事業部研發-呂承翰 <hank.lu@omeco.local>
2023-10-16 13:53:34 +0800

    HK-完成組合模式

--- 組合模式.py
+++ 組合模式.py
@@ -1,4 +1,4 @@
-from abc import abstractclassmethod,ABC
+from abc import abstractmethod,ABC
 
 class Component(ABC):
     _name : str
@@ -7,15 +7,15 @@ class Component(ABC):
         super().__init__()
         self._name = name
     
-    @abstractclassmethod
+    @abstractmethod
     def add(self,val: 'Component'):
         pass
 
-    @abstractclassmethod
+    @abstractmethod
     def remove(self, val: 'Component'):
         pass
 
-    @abstractclassmethod
+    @abstractmethod
     def display(self, val):
         pass
 
@@ -23,3 +23,71 @@ class Leaf(Component):
 
     def __init__(self, name) -> None:
         super().__init__(name)
+
+    def display(self, val):
+        display = '-' * val
+        print(display + self._name)
+    
+    def add(self, val: Component):
+        pass
+
+    def remove(self, val: Component):
+        pass
+
+class Composite(Component):
+    
+
+    def __init__(self, name) -> None:
+        super().__init__(name)
+        self.component_list = []
+  
+
+    def add(self,val: Component):
+        self.component_list.append(val)
+
+    def remove(self, val: Component):
+        self.component_list.remove(val)
+
+    def display(self, val):
+        display = '-' * val
+        print(display + self._name)
+        for i in self.component_list:
+            i.display(val + 2)
+
+if __name__ == "__main__":
+    root = Composite('root')
+    root.add(Leaf('Leaf A'))
+    root.add(Leaf('Leaf B'))
+
+    comp = Composite('Composite X')
+    comp.add(Leaf('Leaf XA'))
+    comp.add(Leaf('Leaf XB'))
+    
+    comp2 = Composite('Composite Xy')
+    comp2.add(Leaf('ab'))
+    comp2.add(Leaf('cb'))
+    comp.add(comp2)
+    root.add(comp)
+
+    root.display(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
