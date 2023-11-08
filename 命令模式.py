8f828aac5609d5883e2eec8065d54f92f2ed94a7
RD2生醫事業部研發-呂承翰 <hank.lu@omeco.local>
2023-11-08 13:31:44 +0800

    HK-命令模式新增加多次undo

--- 命令模式.py
+++ 命令模式.py
@@ -13,15 +13,21 @@ class AbstractCommand(ABC):
 class AddHandler:
     num: int = 0
     value: int = 0
+    undo_list: list = []
 
     def add(self,value: int):
         self.value = value
         self.num += self.value
+        self.undo_list.append(value)
         return self.num
     
     def undo(self):
-        self.num -= self.value
+        if self.undo_list:
+            undo_value = self.undo_list.pop()
+            self.num -= undo_value
         return self.num
+        # self.num -= self.value
+        # return self.num
 
 class AddCommand(AbstractCommand):
     add_handler = AddHandler()
@@ -48,6 +54,7 @@ if __name__ == "__main__":
     calculator = CalculatorForm()
     add_command = AddCommand()
     calculator.set_command(add_command)
-    calculator.compute(5)
-    calculator.compute(10)
+    # calculator.compute(5)
+    # calculator.compute(10)
+    print(calculator.undo())
     print(calculator.undo())
