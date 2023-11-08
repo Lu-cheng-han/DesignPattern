5b902ff9e23d22a28e199e86fcd13be573491fa0
RD2生醫事業部研發-呂承翰 <hank.lu@omeco.local>
2023-11-08 10:34:28 +0800

    HK-新增命令模式

---
+++ 命令模式.py
@@ -0,0 +1,53 @@
+from abc import ABC,abstractmethod
+
+class AbstractCommand(ABC):
+
+    @abstractmethod
+    def execute(self, value: int):
+        pass
+
+    @abstractmethod
+    def undo(self):
+        pass
+
+class AddHandler:
+    num: int = 0
+    value: int = 0
+
+    def add(self,value: int):
+        self.value = value
+        self.num += self.value
+        return self.num
+    
+    def undo(self):
+        self.num -= self.value
+        return self.num
+
+class AddCommand(AbstractCommand):
+    add_handler = AddHandler()
+
+    def execute(self, value: int):
+        return self.add_handler.add(value)
+    
+    def undo(self):
+        return self.add_handler.undo()
+
+class CalculatorForm:
+    command: AbstractCommand
+    
+    def set_command(self, command: AbstractCommand):
+        self.command = command
+    
+    def compute(self,val:int):
+        return  self.command.execute(val)
+
+    def undo(self):
+        return self.command.undo()
+
+if __name__ == "__main__":
+    calculator = CalculatorForm()
+    add_command = AddCommand()
+    calculator.set_command(add_command)
+    calculator.compute(5)
+    calculator.compute(10)
+    print(calculator.undo())
